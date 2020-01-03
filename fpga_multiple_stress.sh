e5b554ed794d5de3f2a539194f57ff8d9251e54b
kidfang <50686062+kidfang@users.noreply.github.com>
2020-01-03 14:01:24 +0800

    Update fpga_multiple_stress.sh

--- fpga_multiple_stress.sh
+++ fpga_multiple_stress.sh
@@ -1,6 +1,9 @@
 ##############################################
 
-fpga_stress_percent=20
+fpga_sram_percent=20
+fpga_dsp_percent=20
+fpga_fabric_percent=20
+
 fpga_d=0b2b
 fpga_tool_path=/root/fpga_scripts    # Be sure this path have fpga_single_stress.sh
 
@@ -12,5 +15,5 @@ for (( i=1; i<=$fpga_n; i=i+1 ));
 
 	do
 		fpga_bp=$(lspci | grep $fpga_d | sed -n "$i"p | cut -f 1 -d " ")
-		gnome-terminal -t "$fpga_bp" -- $fpga_tool_path/fpga_single_stress.sh $fpga_bp $fpga_stress_percent
+		gnome-terminal -t "$fpga_bp" -- $fpga_tool_path/fpga_single_stress.sh $fpga_bp $fpga_sram_percent $fpga_dsp_percent $fpga_fabric_percent
 	done
