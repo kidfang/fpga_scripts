391d861f310338e097babb7cee671024020a2c77
kidfang <50686062+kidfang@users.noreply.github.com>
2020-01-14 09:49:55 +0800

    Update fpga_diag.sh

--- fpga_diag.sh
+++ fpga_diag.sh
@@ -21,16 +21,6 @@ for (( i=1; i<=$w; i=i+1 ));
 		bus_n=$(lspci | grep $fpga_d | sed -n "$i"p)
                 bus_p=$(lspci | grep $fpga_d | sed -n "$i"p | cut -f 1 -d ":")
 
-		### Diagnostics with AFU ###
-
-                source $tool_p/intelrtestack/init_env.sh >/dev/null 2>&1
-                sudo sh -c "echo 2 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages"
-
-                sudo fpgabist -B "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/dma_afu/bin/dma_afu.gbs | tee $result_p/fpga_diag/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt
-		
-                echo $bus_n | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
-                cat $result_p/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt | grep -i Bandwidth | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
-
 		### Diagnostics with nlb_mode_3 ###
 
 		hu_nu=$((20*$w))
@@ -39,6 +29,17 @@ for (( i=1; i<=$w; i=i+1 ));
 		
 		sudo fpgabist -B "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/nlb_mode_3/bin/nlb_mode_3.gbs | tee $result_p/fpga_diag/Diag_nlb_mode_3/diag_"$bus_p"_dma_nlb_mode_3_log.txt
 
+		### Diagnostics with AFU ###
+
+                source $tool_p/intelrtestack/init_env.sh >/dev/null 2>&1
+		
+		########################## Block below command when test arria fpga card #################
+                sudo sh -c "echo 2 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages"
+		##########################################################################################
+		
+                sudo fpgabist -B "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/dma_afu/bin/dma_afu.gbs | tee $result_p/fpga_diag/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt
+		
+                echo $bus_n | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
+                cat $result_p/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt | grep -i Bandwidth | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
+
        done
-
-
