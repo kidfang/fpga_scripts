bae777afb60640cc696a0241c74e73e0f1fd193f
kidfang <50686062+kidfang@users.noreply.github.com>
2020-02-03 11:21:03 +0800

    Update fpga_diag.sh

--- fpga_diag.sh
+++ fpga_diag.sh
@@ -1,7 +1,5 @@
 #############################################################
 
-fpga_d=0b2b
-#fpga_d=09c4
 result_p=/root
 
 #############################################################
@@ -10,36 +8,57 @@ mkdir $result_p/fpga_diag >/dev/null 2>&1
 mkdir $result_p/fpga_diag/Diag_AFU >/dev/null 2>&1
 mkdir $result_p/fpga_diag/Diag_nlb_mode_3 >/dev/null 2>&1
 
-tool_p=/home/d5005
-#tool_p=/home/a10gx
+fpga_s=$( lspci | grep -i 0b2b | wc -l )
+fpga_a=$( lspci | grep -i 09c4 | wc -l )
 
-w=$( lspci | grep -i $fpga_d | wc -l )
-#w=1
+if [ $fpga_s -gt 0 ]; then
+        tool_p=/home/d5005
+        fpga_d=0b2b
+        fpga_n=$fpga_s
+        fpga_parm=B
+        w=$fpga_s
+        afu=2
+        afu_hpage=1048576
+        afu_check=Bandwidth
 
-for (( i=1; i<=$w; i=i+1 ));
-	do
-		bus_n=$(lspci | grep $fpga_d | sed -n "$i"p)
+elif [ $fpga_a -gt 0 ]; then
+        tool_p=/home/a10gx
+        fpga_d=09c4
+        fpga_n=$fpga_a
+        fpga_parm=b
+        w=1
+        afu=20
+        afu_hpage=2048
+        afu_check=bandwidth
+
+else
+        echo "No FPGA device, can not test!!"
+        exit
+fi
+
+#############################################################
+
+for (( i=1; i<=$fpga_n; i=i+1 ));
+        do
+                bus_n=$(lspci | grep $fpga_d | sed -n "$i"p)
                 bus_p=$(lspci | grep $fpga_d | sed -n "$i"p | cut -f 1 -d ":")
 
-		### Diagnostics with nlb_mode_3 ###
+                ### Diagnostics with nlb_mode_3 ###
 
-		hu_nu=$((20*$w))
-		source $tool_p/intelrtestack/init_env.sh >/dev/null 2>&1
-		sudo sh -c "echo $hu_nu > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages"
-		
-		sudo fpgabist -B "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/nlb_mode_3/bin/nlb_mode_3.gbs | tee $result_p/fpga_diag/Diag_nlb_mode_3/diag_"$bus_p"_dma_nlb_mode_3_log.txt
+                hu_nu=$((20*$w))
+                source $tool_p/intelrtestack/init_env.sh >/dev/null 2>&1
+                sudo sh -c "echo $hu_nu > /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages"
 
-		### Diagnostics with AFU ###
+                sudo fpgabist -"$fpga_parm" "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/nlb_mode_3/bin/nlb_mode_3.gbs | tee $result_p/fpga_diag/Diag_nlb_mode_3/diag_"$bus_p"_dma_nlb_mode_3_log.txt
+
+                ### Diagnostics with AFU ###
 
                 source $tool_p/intelrtestack/init_env.sh >/dev/null 2>&1
-		
-		########################## Block below command when test arria fpga card #################
-                sudo sh -c "echo 2 > /sys/kernel/mm/hugepages/hugepages-1048576kB/nr_hugepages"
-		##########################################################################################
-		
-                sudo fpgabist -B "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/dma_afu/bin/dma_afu.gbs | tee $result_p/fpga_diag/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt
-		
+                sudo sh -c "echo $afu > /sys/kernel/mm/hugepages/hugepages-"$afu_hpage"kB/nr_hugepages"
+
+                sudo fpgabist -"$fpga_parm" "$bus_p" $OPAE_PLATFORM_ROOT/hw/samples/dma_afu/bin/dma_afu.gbs | tee $result_p/fpga_diag/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt
+
                 echo $bus_n | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
-                cat $result_p/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt | grep -i Bandwidth | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
+                cat $result_p/fpga_diag/Diag_AFU/diag_"$bus_p"_dma_fau_log.txt | grep -i $afu_check | tee -a $result_p/fpga_diag/Diag_AFU/fpga_dma_fau_check.txt
 
        done
